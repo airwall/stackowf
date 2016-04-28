576169fde4258b2444f06e8d2986a33206b448dc
Belinschi Dima <belinski.d@gmail.com>
2016-04-28 17:31:51 +0300

    add views | add questions function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,18 +3,16 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
 
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @question, notice: "Answer was successfully added." }
-      else
-        format.html { redirect_to @question, notice: "Fill all inputs of answer" }
-      end
+    if @answer.save
+      redirect_to @question, notice: "Answer was successfully added."
+    else
+      redirect_to @question, notice: "Fill all inputs of answer"
     end
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:body, :qustion_id)
+    params.require(:answer).permit(:body, :question_id)
   end
 end
