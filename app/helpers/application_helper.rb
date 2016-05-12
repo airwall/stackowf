56cc17923fc3de1a1fc307ff746c512a42c2db1c
Belinschi Dima <belinski.d@gmail.com>
2016-05-12 14:06:03 +0300

    Refactoring | Add helper render delete/edit object

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def username(id)
-    id != nil ? username = User.find(id).email : username = "%username%"
+    username = !id.nil? ? User.find(id).email : "%username%"
+  end
+
+  def views_action(object)
+    if user_signed_in? && current_user.author_of?(object)
+      render "views_action", object: object
+    end
   end
 end
