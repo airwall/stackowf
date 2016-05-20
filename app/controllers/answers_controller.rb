68f239365eb173099a7093542549ccda38ff8f3b
Belinschi Dima <belinski.d@gmail.com>
2016-05-20 19:44:18 +0300

    add test to method PATCH answers_controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :get_question, only: [:create]
-  before_action :get_answer, only: [:edit, :destroy]
+  before_action :get_answer, only: [:edit, :destroy, :update]
 
   def create
     @answer = @question.answers.new(answer_params.merge(user: current_user))
@@ -9,6 +9,11 @@ class AnswersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    @answer.update(answer_params) if current_user.author_of?(@answer)
+
   end
 
   def destroy
