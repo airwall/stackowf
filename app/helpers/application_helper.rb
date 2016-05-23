d9d9791d364be2358f25e22f0fc92f38f14d9cd0
Belinschi Dima <belinski.d@gmail.com>
2016-05-23 18:23:22 +0300

    add helper show_link?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     assign.user.try(:username) || "%username%"
   end
 
+  def show_link?(object)
+    user_signed_in? && current_user.author_of?(object)
+  end
+
   def views_action(object)
     if user_signed_in? && current_user.author_of?(object)
       render "layouts/views_action", object: object
