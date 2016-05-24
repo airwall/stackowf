2491621f8114ad728b614160710d6a3f73af9405
Belinschi Dima <belinski.d@gmail.com>
2016-05-24 23:17:55 +0300

    change migrate add column best to answers

--- db/migrate/20160523184059_add_null_false_to_best_column_of_answers.rb
+++ db/migrate/20160523184059_add_null_false_to_best_column_of_answers.rb
@@ -1,5 +1,5 @@
 class AddNullFalseToBestColumnOfAnswers < ActiveRecord::Migration[5.0]
   def change
-    change_column :answers, :best, :boolean, default: false, null: false
+    add_column :answers, :best, :boolean, default: false, null: false
   end
 end
