683ecfd6a6e727169ba8f1b54145f106b6a98739
Belinschi Dima <belinski.d@gmail.com>
2016-06-08 18:06:22 +0300

    push to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require bootstrap-sprockets
 //= require cocoon
 //= require turbolinks
+//= require polifyl
 //= require_tree .
--- app/jobs/answer_job.rb
+++ app/jobs/answer_job.rb
@@ -1,5 +1,6 @@
 class AnswerJob < ApplicationJob
   def perform(answer)
+    # binding.pry
     ActionCable.server.broadcast "questions:#{answer.question_id}:answers",
      answer: AnswersController.render(partial: 'answers/answer', locals: { answer: answer })
   end
