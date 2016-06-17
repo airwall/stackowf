4574027e7c0d2ba03ea65755cf6b6a8b8163a8e8
Belinschi Dima <belinski.d@gmail.com>
2016-06-17 21:09:07 +0300

    add database cleaner in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,36 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+
+  #======================
+  config.before(:suite) do
+    # Truncate database to clean up garbage from
+    # interrupted or badly written examples
+    DatabaseCleaner.clean_with(:truncation)
+
+    # Seed dataase. Use it only for essential
+    # to run application data.
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  config.around(:each) do |example|
+    # Use really fast transaction strategy for all
+    # examples except `js: true` capybara specs
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+
+    # Start transaction
+    DatabaseCleaner.cleaning do
+      # Run example
+      example.run
+    end
+
+    load "#{Rails.root}/db/seeds.rb" if example.metadata[:js]
+
+    # Clear session data
+    Capybara.reset_sessions!
+  end
+  #=======================
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
