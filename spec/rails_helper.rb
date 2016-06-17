a0d418e0b8f9c61663cc5ba7102d9b2e29f6ca3d
Belinschi Dima <belinski.d@gmail.com>
2016-06-17 22:13:30 +0300

    filter rails from backtrace!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -92,6 +92,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
 end
 
 Shoulda::Matchers.configure do |config|
