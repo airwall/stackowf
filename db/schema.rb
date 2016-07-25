dc39ebfbf63851b17bc7422a7ac407391ede0db0
Belinschi Dima <belinski.d@gmail.com>
2016-07-25 20:28:38 +0300

    add model Search and test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720171442) do
+ActiveRecord::Schema.define(version: 20160725143211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,11 @@ ActiveRecord::Schema.define(version: 20160720171442) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subscriptions", force: :cascade do |t|
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,19 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+
+  config.include SphinxHelpers, type: :feature
+  config.include SphinxHelpers, type: :sphinx
+
+  config.before(:suite) do
+    # Ensure sphinx directories exist for the test environment
+    ThinkingSphinx::Test.init
+    # Configure and start Sphinx, and automatically
+    # stop Sphinx at the end of the test suite.
+    ThinkingSphinx::Test.start_with_autostop
+  end
 
   #======================
   config.before(:suite) do
@@ -43,6 +55,11 @@ RSpec.configure do |config|
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :sphinx) do
+    # For tests tagged with Sphinx, use deletion (or truncation)
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, type: :feature) do
