8bfacedd2a731dc4408f1e8da871c5742201d9bc
Belinschi Dima <belinski.d@gmail.com>
2016-07-27 23:21:28 +0300

    deploy test 2

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+# config valid only for current version of Capistrano
+lock '3.6.0'
+
+set :application, 'stackowf'
+set :repo_url, 'git@github.com:airwall/stackowf.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/airwall/stackowf'
+set :scm, :git
+set :deploy_user, 'airwall'
+set :tmp_dir, "/home/airwall/tmp"
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml, .env'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+  after :publishing, :restart
+end
