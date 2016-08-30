9b0982718919f9caa96087a3a4a59b6417f2196e
Belinschi Dima <belinski.d@gmail.com>
2016-08-30 16:01:03 +0300

    add caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymorphic: true, optional: true
+  belongs_to :commentable, polymorphic: true, optional: true, touch: true
   belongs_to :user, optional: true
 
   validates :user_id, :commentable_id, :commentable_type, :body, presence: true
+
+  default_scope -> { order(:created_at => :asc) }
 
   after_commit { CommentJob.perform_later(self) }
 end
