b84401a400a50b4b05b949c7a87e5550261082d5
Belinschi Dima <belinski.d@gmail.com>
2016-10-03 20:22:02 +0300

    setup carrierwave-aws

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    @question.update!(question_params)
+    @question.update(question_params)
     respond_with @question
   end
 
